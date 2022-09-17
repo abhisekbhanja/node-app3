2f4ea4a6ee6e108ac5cc13f3095a17aeb30f21b5
abhisekbhanja <abhisekbhanja98@yahoo.com>
2022-09-17 22:29:08 +0530

    add route to the page

--- server.js
+++ server.js
@@ -4,6 +4,9 @@ const port=process.env.PORT || 5000
 app.get("/",(req,res)=>{
     res.send("app is ruuning")
 })
+app.get("/about",(req,res)=>{
+    res.send("about page")
+})
 
 app.listen(port,()=>{
     console.log("server is ruuning at port :"+port);
